5ec4314909ff02b347304532dea2df0c5365bca3
lwyj123 <liang.pearce@gmail.com>
2019-04-15 19:38:18 +0800

    feat: init

---
+++ codesmith.config.js
@@ -0,0 +1,32 @@
+'use strict';
+const path = require('path');
+
+module.exports = function (codesmith) {
+    codesmith.setGenerator('simple-stylelint', {
+        description: 'create stylelint for 🚧💩',
+        questions: [
+            {
+                type: 'checkbox',
+                name: 'extends',
+                message: 'What config standard do you like?',
+                choices: [
+                    {name: 'stylelint-config-standard', value: 'stylelint-config-standard', checked: true},
+                ]
+            }
+        ],
+        actions: [
+            `this is a comment`,
+            'this is another comment',
+            {
+                type: 'add',
+                path: 'stylelint.config.js',
+                templateFile: 'templates/stylelint.config.js',
+                abortOnFail: true
+            },
+            {
+                type: 'shell',
+                command: 'yarn add stylelint-config-standard'
+            }
+        ]
+    });
+};
---
+++ templates/stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: [
+    'stylelint-config-standard',
+  ],
+  rules: {
+    "no-descending-specificity": null,
+  }
+};
